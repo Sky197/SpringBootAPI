c3fa226537cad604bb32b4d93bc5d0c2a0e2ab58
fernando <generatof@gmail.com>
2020-05-11 14:00:20 -0300

    configurando heroku

--- src/main/java/xyz/dev/config/DatabaseConfig.java
+++ src/main/java/xyz/dev/config/DatabaseConfig.java
@@ -1,14 +1,22 @@
 package xyz.dev.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 @Configuration
 public class DatabaseConfig {
@@ -22,6 +30,9 @@ public class DatabaseConfig {
     @Value("${spring.datasource.driver-class-name}")
     private String driver;
 
+    @Value("${packages-to-scan}")
+    private String packages;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSourceBuilder = new DriverManagerDataSource();
@@ -30,6 +41,25 @@ public class DatabaseConfig {
         dataSourceBuilder.setPassword(password);
         dataSourceBuilder.setUrl(url);
         return dataSourceBuilder;
+    }
+
+    @Autowired
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(packages);
+        JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(jpaVendorAdapter);
+        return em;
+    }
+
+
+    @Bean
+    PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(emf);
+        return jpaTransactionManager;
     }
 
 
